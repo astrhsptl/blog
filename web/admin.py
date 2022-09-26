25fa0e294cda59f8b8b6f0a3849ef1a24e46aad2
astrhsptl <dctrback@bk.ru>
2022-09-26 05:19:01 -0400

    add News model, registrate the model in django admin

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import News
 
-# Register your models here.
+
+class NewsAdmin(admin.ModelAdmin):
+    fields = (
+        'id', 'title', 'content', 'created', 'updated', 'publicated'
+    )
+    list_editable = ('publicated', )
+    list_display = (
+        'id', 'title', 'created', 'updated', 'publicated'
+    )
+    search_fields = (
+        'id', 'title', 'created', 'updated', 'publicated'
+    )
+    readonly_fields = (
+        'id', 'created', 'updated'
+    )
+
+admin.site.register(News, NewsAdmin)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class News(models.Model):
+    title = models.CharField(
+        max_length=255, verbose_name='title'
+    )
+    content = models.TextField(
+        max_length=4096, verbose_name='content'
+    )
+    created = models.DateField(
+        auto_now=True, verbose_name='created'
+    )
+    updated = models.DateField(
+        auto_now_add=True, verbose_name='updated'
+    )
+    publicated = models.BooleanField(
+        default=False
+    )
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('news', kwargs={'pk': self.pk})
